285ea21b959c3b8cb90ca2be03cd91d1c0fe180c
xie186 <xieshaojun0526@gmail.com>
2025-04-17 02:31:11 +0000

    rm data/*wav

--- scripts/test_kokoro.py
+++ scripts/test_kokoro.py
@@ -7,14 +7,22 @@ fallback = espeak.EspeakFallback(british=False)
 g2p = en.G2P(trf=False, british=False, fallback=fallback)
 
 # Kokoro
-kokoro = Kokoro("kokoro-v0_19.onnx", "voices.bin")
+kokoro = Kokoro("kokoro-v1.0.onnx", "voices-v1.0.bin")
 
 # Phonemize
-text = """
-     [Misaki](/misˈɑki/) is a G2P engine designed for [Kokoro](/kˈOkəɹO/) models."""
+#text = """
+#     [Misaki](/misˈɑki/) is a G2P engine designed for [Kokoro](/kˈOkəɹO/) models."""
+
+#text = "A [FASTQ](/ˈfˈæstkju/) file"
+#text = "fast"
+#text = "Today, we'll break down what Phred quality scores are, how they're calculated, and how they're encoded in [FASTQ](/fæstˈkjuː/) files."
+#text = "Today, we'll break down what Phred quality scores are, how they're calculated, and how they're encoded in [FASTQ](/ˈfˈæstkju/) files."
+text = "Let us fectch the data"
+    
 phonemes, _ = g2p(text)
-print(dir(kokoro))
-print(kokoro.voices.files)
+print(phonemes)
+#print(dir(kokoro))
+#print(kokoro.voices.files)
 
 for voice in kokoro.voices.files:
     # Create
